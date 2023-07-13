e94de95a546955b2f2f2d7f221f85fbafaa27e20
Flavio Gouvea <flaviogouveaneto@gmail.com>
2023-07-13 01:51:34 -0300

    fix: fix page component

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { Book } from "../types/book";
 export default async function Page() {
   const data: any = await getLatestData();
 
-  const dataFiction: Book[] = data ? data.results.lists[0].books : undefined;
+  const dataFiction: Book[] = data ? data.results?.lists[0]?.books : undefined;
 
   return (
     <div className="book-list">{dataFiction && dataFiction.map((book, i) => <BookCard book={book} key={i} />)}</div>
