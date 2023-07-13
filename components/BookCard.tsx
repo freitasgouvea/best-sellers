2f1b888d511a624948d9683cda5b484779c945d1
Flavio Gouvea <flaviogouveaneto@gmail.com>
2023-07-13 13:31:20 -0300

    chore: add info icon tooltip and update book card

--- components/BookCard.tsx
+++ components/BookCard.tsx
@@ -1,23 +1,43 @@
 import styles from "../styles/BookCard.module.css";
 import { Book } from "../types/book";
+import { InfoIcon } from "./InfoIcon";
 
 export const BookCard = ({ book }: { book: Book }) => {
-  const { rank, title, contributor, description, book_image: BookImage, buy_links: BuyLinks } = book;
+  const {
+    rank,
+    title,
+    contributor,
+    description,
+    book_image: BookImage,
+    buy_links: BuyLinks,
+    weeks_on_list: weeksOnList,
+    rank_last_week: rankLastWeek,
+  } = book;
+
+  const rankVariation = rankLastWeek - rank;
+
+  const infoIconType = rankLastWeek === 0 ? "new" : rankVariation > 0 ? "up" : rankVariation < 0 ? "down" : "equal";
+
   return (
     <div className={styles.card}>
-      <div className={styles.cardRank}>
-        <div className="tooltip">
+      <div className={styles.CardHeader}>
         <div className={styles.rank}># {rank}</div>
-          <span className="tooltiptext">Tooltip text</span>
+        <div className="tooltip">
+          <InfoIcon type={infoIconType} size={24} />
+          <span className="tooltiptext">
+            <small>Weeks On List: {weeksOnList} </small>
+            <br />
+            <small>Rank Last Week: {rankLastWeek} </small>
+          </span>
         </div>
       </div>
-      <div className={styles.cardHeader}>
-        <div className={styles.cardHeaderInfo}>
+      <div className={styles.cardContent}>
+        <div className={styles.cardContentInfo}>
           <p className={styles.title}>{title}</p>
           <p className={styles.author}>{contributor}</p>
           <p className={styles.description}>{description}</p>
         </div>
-        <div className={styles.cardHeaderCover}>
+        <div className={styles.cardContentCover}>
           <img className={styles.bookCover} src={BookImage} alt="Book Cover" />
         </div>
       </div>
