6786f03b281b85e7e1d9f8173e06284e336b8f03
Flavio Gouvea <flaviogouveaneto@gmail.com>
2023-07-13 01:46:51 -0300

    fix: fix lint

--- components/BookCard.tsx
+++ components/BookCard.tsx
@@ -1,33 +1,30 @@
 import styles from "../styles/BookCard.module.css";
 import { Book } from "../types/book";
 
-type BookCardProps = {
-  book: Book;
-};
-
-export const BookCard: React.FC<BookCardProps> = ({ book }) => {
+export const BookCard = ({ book }: { book: Book }) => {
+  const { rank, title, contributor, description, book_image: BookImage, buy_links: BuyLinks } = book;
   return (
     <div className={styles.card}>
       <div className={styles.cardRank}>
-        <div className={styles.rank}># {book.rank}</div>
+        <div className={styles.rank}># {rank}</div>
       </div>
       <div className={styles.cardHeader}>
         <div className={styles.cardHeaderInfo}>
-          <p className={styles.title}>{book.title}</p>
-          <p className={styles.author}>{book.contributor}</p>
-          <p className={styles.description}>{book.description}</p>
+          <p className={styles.title}>{title}</p>
+          <p className={styles.author}>{contributor}</p>
+          <p className={styles.description}>{description}</p>
         </div>
         <div className={styles.cardHeaderCover}>
-          <img className={styles.bookCover} src={book.book_image} alt="Book Cover" />
+          <img className={styles.bookCover} src={BookImage} alt="Book Cover" />
         </div>
       </div>
       <div className={styles.cardFooter}>
         <div className="dropdown">
           <button className={styles.cardButton}>Buy</button>
           <div className="dropdown-content">
-            {book.buy_links &&
-              book.buy_links.map((item) => (
-                <a href={item.url} target="blank">
+            {BuyLinks &&
+              BuyLinks.map((item, key) => (
+                <a key={key} href={item.url} target="blank">
                   {item.name}
                 </a>
               ))}
