84750be1cb6c6a129bd7657e292e24fd04c1105a
Flavio Gouvea <flaviogouveaneto@gmail.com>
2023-07-17 00:45:55 -0300

    chore: add metadata icons and sitemap

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,32 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "NYTBS",
   description: "The New York Times Best Sellers",
+  openGraph: {
+    title: "NYT Best Sellers",
+    description: "The New York Times Best Sellers",
+    url: "https://freitasgouvea.github.io/best-sellers",
+    siteName: "NYTBS",
+    locale: "en_US",
+    type: "website",
+  },
+  robots: {
+    index: false,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+    maximumScale: 1,
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
