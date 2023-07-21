3299c3d7f8695336370d42d9a706affc7598dd78
Flavio Gouvea <flaviogouveaneto@gmail.com>
2023-07-21 01:22:33 -0300

    chore: add nyt logo and link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,9 @@ import styles from "../styles/Footer.module.css";
 export const Footer = () => {
   return (
     <footer className={styles.pageFooter}>
-      <p>Created by Flavio Gouvea</p>
+      <a href="https://developer.nytimes.com" target="blank">
+        <img src="https://developer.nytimes.com/files/poweredby_nytimes_200c.png?v=1583354208354" />
+      </a>
     </footer>
   );
 };
