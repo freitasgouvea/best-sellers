f8e156a065152bb1b25146c7673b9696ae133773
Flavio Gouvea <flaviogouveaneto@gmail.com>
2023-07-21 01:35:33 -0300

    test: update footer test

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,9 +3,10 @@ import styles from "../styles/Footer.module.css";
 export const Footer = () => {
   return (
     <footer className={styles.pageFooter}>
-      <a href="https://developer.nytimes.com" target="blank">
+      <a href="https://developer.nytimes.com" target="blank" data-testid="nyt-link">
         <img src="https://developer.nytimes.com/files/poweredby_nytimes_200c.png?v=1583354208354" />
       </a>
+      <br />
     </footer>
   );
 };
